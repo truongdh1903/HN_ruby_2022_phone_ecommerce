0bff6878e9c314fae16a87141beaa5ed549e0f12
lemanhcuongSun <le.manh.cuong-b@sun-asterisk.com>
2022-01-20 01:05:25 +0700

    Add template website

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def cart; end
+
+  def checkout; end
+
+  def shop; end
+
+  def product; end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,22 @@
 // This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
+// present in this directory. You"re encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// that code so it"ll be compiled.
+
+require("jquery")
+import "bootstrap"
+require("../lib/owl.carousel.min")
+require("../lib/jquery.sticky")
+require("../lib/jquery.easing.1.3.min")
+require("./main")
+require("../lib/bxslider.min")
+require("./script.slider")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Rails.start()
 Turbolinks.start()
