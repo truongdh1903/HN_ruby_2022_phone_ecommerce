7f53238102430659368d13efcaaf9a958b8240c9
= <=>
2022-03-06 11:49:59 +0700

    Cart ajax

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,12 @@ module CartsHelper
   def get_product_detail id
     @product_detail = ProductDetail.find_by id: id
   end
+
+  def product_checkbox checked
+    if checked
+      "<input type='checkbox' class='product-checkbox checked'".html_safe
+    else
+      "<input type='checkbox' class='product-checkbox'".html_safe
+    end
+  end
 end
